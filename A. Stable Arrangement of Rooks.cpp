db5180d1570fd366c932e0dda48b61a377c57cbb
Anas jaidi <anas.jaidi@icloud.com>
2022-01-04 21:48:17 +0100

    finish day work

---
+++ A. Stable Arrangement of Rooks.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void solve()
+{
+    int x , y , c = 0;
+    cin >> x >> y;
+    for (size_t i = 0; i < x; i++)
+    {
+       for (size_t j = 0; j < x; j++)
+       {
+        //    if (abs(i - j)> 1)
+        //         c++;
+       }
+       
+    }
+    cout << c << endl;
+}
+
+
+int main()
+{
+    int t ;
+    //cin >> t;
+    cout << t ;
+    // while (t != 0)
+    // {
+    //     solve();
+    //     t--;
+    // }
+}
